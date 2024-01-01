d73b2acede17edc38f5d4955d71c1750204f2a89
Jacqueline <jacquigavi921@gmail.com>
2024-01-01 18:55:36 -0500

    modificacion de rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD:src/app/app.component.ts
-  title = 'proyecto_FrontEnd';
-=======
   title = 'loginApp';
->>>>>>> 057b748fc7ead57965575b5ce8964536fc499e58:bancaWeb/src/app/app.component.ts
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/app/app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
@@ -16,22 +15,10 @@ import { ProductoCreditoComponent } from './producto-credito/producto-credito.co
 import { ProductoCuentasComponent } from './producto-cuentas/producto-cuentas.component';
 import { ProductosComponent } from './productos/productos.component';
 import { SolicitudCreditoComponent } from './solicitud-credito/solicitud-credito.component';
-=======
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-import { RegistroComponent } from './registro/registro.component';
-import { InicioComponent } from './inicio/inicio.component';
-import { LoginComponent } from './login/login.component';
->>>>>>> 057b748fc7ead57965575b5ce8964536fc499e58:bancaWeb/src/app/app.module.ts
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD:src/app/app.module.ts
     OpcionesComponent,
     RegistrarseComponent,
     DetallePagoCuentaComponent,
@@ -47,15 +34,6 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,
     RouterModule,
     AppRoutingModule
-=======
-    RegistroComponent,
-    InicioComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
->>>>>>> 057b748fc7ead57965575b5ce8964536fc499e58:bancaWeb/src/app/app.module.ts
   ],
   providers: [],
   bootstrap: [AppComponent]
